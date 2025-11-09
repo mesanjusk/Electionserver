2190e38d0623544ecefed36b82c4f842f60e77e6
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-11-09 21:26:09 +0530

    12345

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 
 export async function connectDB(uri) {
   mongoose.set('strictQuery', true);
-  await mongoose.connect(uri, { dbName: 'voter_search' });
+  await mongoose.connect(uri, { dbName: 'Election' });
   console.log('MongoDB connected');
 }
